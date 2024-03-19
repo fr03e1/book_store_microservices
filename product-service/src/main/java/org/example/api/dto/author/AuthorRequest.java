05e533324289b4386a6889b2c087c451419aca31
a.bocharov <a.bocharov@msocialproduction.com>
2024-03-19 21:21:20 +0300

    test

--- product-service/src/main/java/org/example/api/dto/author/AuthorRequest.java
+++ product-service/src/main/java/org/example/api/dto/author/AuthorRequest.java
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record AuthorRequest(
-        @NotBlank(message = "Title is required")
+        @NotBlank(message = "Title is required!")
         @Size(max = 255, message = "Name must be at most 255 characters long")
         String name
 ) {
